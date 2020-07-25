ef70d6d057dffb69c83409beda1d82c8657a23bf
ansarali41 <ansar.ict14@gmail.com>
2020-07-26 03:57:33 +0600

    finaly done it ,need 2 days for complete subtotal tax and grandTatal section

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const minusBtn = document.getElementById('minus-btn');
 minusBtn.addEventListener('click', function () {
     var valueNumber = getValue('numbers-of-phone');
 
-    if (valueNumber > 1) {
+    if (valueNumber > 0) {
         valueNumber--;
     }
-    setValue('numbers-of-phone',valueNumber);
-    console.log(valueNumber);
-    PriceSet('phone-price',valueNumber,1219);
+    setValue('numbers-of-phone', valueNumber);
+    PriceSet('phone-price', valueNumber);
 
 })
 
@@ -17,9 +16,9 @@ const plusBtn = document.getElementById('plus-btn');
 plusBtn.addEventListener('click', function () {
     var valueNumber = getValue('numbers-of-phone');
     valueNumber++;
-    setValue('numbers-of-phone',valueNumber);
+    setValue('numbers-of-phone', valueNumber);
     // phone price
-    PriceSet('phone-price',valueNumber,1219);
+    PriceSet('phone-price', valueNumber);
 })
 
 function getValue(id) {
@@ -27,34 +26,53 @@ function getValue(id) {
     var numbersOfPhoneValue = parseFloat(numbersOfPhone);
     return numbersOfPhoneValue;
 }
-function setValue(id,valueNumber) {
+function setValue(id, valueNumber) {
     document.getElementById(id).value = valueNumber;
 }
 
 // phone price set function
-function PriceSet(id,valueNumber,basePrice) {
-    const phonePriceValue=document.getElementById(id).innerText;
-    phonePriceNumber=parseFloat(phonePriceValue);
-    document.getElementById(id).innerText=basePrice*valueNumber;
+function PriceSet(id, valueNumber) {
+    const phonePriceValue = document.getElementById(id).innerText;
+    phonePriceNumber = parseFloat(phonePriceValue);
+
+    if (id == 'phone-price') {
+        document.getElementById(id).innerText = 1219 * valueNumber;
+    }
+    else if (id == 'case-price') {
+        document.getElementById(id).innerText = 59 * valueNumber;
+    }
+    const caseSubTotal=parseInt(document.getElementById('case-price').innerText);
+    const phoneSubTotal =parseInt(document.getElementById('phone-price').innerText);
+    const subTotal = caseSubTotal + phoneSubTotal;
+    document.getElementById('subTotal').innerText=subTotal;
+    // tax  
+    const tax =(caseSubTotal/59)*1 +(phoneSubTotal/1219)*1;
+    document.getElementById('tax').innerText=tax;
+
+    // grand Total 
+    const grandTotal=subTotal+tax;
+    document.getElementById('grand-total').innerText=grandTotal;
+
+
 }
-             // case card section
+// case card section
 //case event handler minus btn
 const caseMinusBtn = document.getElementById('case-minus-btn');
-caseMinusBtn.addEventListener('click',function(){
+caseMinusBtn.addEventListener('click', function () {
     var caseValueNumber = getValue('case-value');
-    if (caseValueNumber>1) {
+    if (caseValueNumber > 0) {
         caseValueNumber--;
     }
-    setValue('case-value',caseValueNumber);
-    PriceSet('case-price',caseValueNumber,59);
+    setValue('case-value', caseValueNumber);
+    PriceSet('case-price', caseValueNumber);
 
 })
 // case event handler plus btn
 const casePlusBtn = document.getElementById('case-plus-btn');
-casePlusBtn.addEventListener('click',function(){
-    var caseValueNumber=getValue('case-value');
+casePlusBtn.addEventListener('click', function () {
+    var caseValueNumber = getValue('case-value');
     caseValueNumber++;
-    setValue('case-value',caseValueNumber);
-    PriceSet('case-price',caseValueNumber,59);
+    setValue('case-value', caseValueNumber);
+    PriceSet('case-price', caseValueNumber);
 })
 
